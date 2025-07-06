3cad37045637b5e0ee048627e6e134ce15a69c0b
skci <xiaokkk@foxmail.com>
2025-07-06 12:09:21 +0800

    feat(refactor): 重构项目以原生适配 Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/cloudflare" {
@@ -9,18 +9,7 @@ declare module "@remix-run/cloudflare" {
 }
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [remix(), tsconfigPaths()],
   ssr: {
     resolve: {
       conditions: ["workerd", "worker", "browser"],
